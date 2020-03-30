effa9a0047b44b78f6ef817d35ac79f3dceadb8d
jsad2023 <jsad@bu.edu>
2020-03-30 18:11:00 +0100

    Fixed the problem

--- TrussFall/comp_analysis/getMaxLoad.m
+++ TrussFall/comp_analysis/getMaxLoad.m
@@ -6,11 +6,11 @@ function max_load = getMaxLoad(T,straw_lengths, C)
 [~, members] = size(C);
 %ask user if whether to use  the euler buckling fit or the empirical fit
 disp('Use empirical fit or euler buckling fit to get theortical max load?')
-method = menu('Choose a method:', 'Euler Buckling Fit', 'Empirical Fit');
-if method == 1
+choice = menu('Choose a method:', 'Euler Buckling Fit', 'Empirical Fit');
+if choice == 1
     A = 1340.9; %puts in paramer
     buckling = A * straw_lengths .^ (-2);
-elseif method == 2
+elseif choice == 2
     A = 197.7; a = 1.238;
     buckling = A * straw_lengths .^ (-a);
 end
