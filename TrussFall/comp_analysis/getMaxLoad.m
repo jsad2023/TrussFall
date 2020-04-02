a1dae9c3f3cf4c591992a8ef06ddd311d1e58115
jsad2023 <jsad@bu.edu>
2020-04-02 04:29:53 +0100

    Fixed straw lengths; Added getBuckling.m

--- TrussFall/comp_analysis/getMaxLoad.m
+++ TrussFall/comp_analysis/getMaxLoad.m
@@ -1,19 +1,15 @@
-function max_load = getMaxLoadNew(T,straw_lengths, C)
+function max_load = GetMaxLoad(T,straw_lengths, C)
 %Calculates theoritical max load of the truss. Will ask user if to use
 %either the euler buckling fit or the empirical fit
 %Format: max_load = getMaxLoad(Forces_in_members, straw_lengths)
  
 [~, members] = size(C);
 %ask user if whether to use  the euler buckling fit or the empirical fit
-disp('Use empirical fit or euler buckling fit to get theortical max load?')
-choice = menu('Choose a method:', 'Euler Buckling Fit', 'Empirical Fit');
-if choice == 1
-    A = 1340.9; %puts in paramer
-    buckling = A * straw_lengths .^ (-2);
-elseif choice == 2
-    A = 197.7; a = 1.238;
-    buckling = A * straw_lengths .^ (-a);
-end
+
+%We're using empircal fit to calculate buckling force
+A = 197.7; a = 1.238;
+buckling = A * straw_lengths .^ (-a);
+
 
 compression_forces = T(1:members);
 compression_forces(find(compression_forces >= 0)) = 0;
